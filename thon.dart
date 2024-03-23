8720e9f4a20376408e66b74ba62b71dd8fb22c8f
ruthcohort2024 <ruthkariuki586@gmail.com>
2024-03-23 15:18:44 +0300

    first commit

---
+++ thon.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Ruth";
+  int age = 25;
+  String school = "PLP Academy";
+  String hobby = "practicing and writting codes";
+  
+  print("My name is $name. I am $age years old and currently attending $school. My hobby is $hobby.");
+}
